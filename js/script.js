b6625e81b4f1a985b1a23e236d277e1c92b528aa
Delfina <delfiespelet11@gmail.com>
2024-05-19 23:50:39 -0300

    formulario de contacto

--- js/script.js
+++ js/script.js
@@ -7,3 +7,16 @@ avisos.forEach(aviso => {
     )
 })
    
+function valFormulario() {
+    let nombre = document.getElementById("nombre").value.trim();
+
+    if (nombre === "" || nombre.lenght == 0) {
+        alert("No puede dejar vacío el espacio Nombre");
+        return false;
+    } else {
+        alert("REGISTRACIÓN exitosa");
+    }
+    }
+
+    const enviarMensaje = () => alert("ATENCIÓN: Esta acción borrará todos los cambos. ¿Confirma?")
+
