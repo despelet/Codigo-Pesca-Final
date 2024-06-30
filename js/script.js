a8830e972acd7ec794bd8bb6e92bf3d32eec0445
despelet <delfiespelet11@gmail.com>
2024-06-30 13:44:45 -0300

    carrito de compras funcional

--- js/script.js
+++ js/script.js
@@ -20,109 +20,20 @@ function valFormulario() {
 
     const enviarMensaje = () => alert("ATENCIÓN: Esta acción borrará todos los cambios. ¿Confirma?")
 
-let cart = [];
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
+// Asegúrate de que el script se ejecute después de que el DOM esté completamente cargado
+document.addEventListener('DOMContentLoaded', () => {
+    updateCart();
+    updateCartCount(); // Inicializar el número de elementos en el carrito
+});
 
-// function addToCart(productName, productPrice) {
-//     const existingProduct = cart.find(item => item.name === productName);
-//     if (existingProduct) {
-//         existingProduct.quantity++;
-//     } else {
-//         cart.push({ name: productName, price: productPrice, quantity: 1 });
-//     }
-//     if(productAdded) {
-    
-//     const avisos = document.querySelectorAll(".aviso");
-//     const productAdded = cart.some(item => item.name === productName);
-
-//     avisos.forEach(aviso => {
-//         aviso.addEventListener("click", (event)=> {
-//             event.preventDefault();
-//             alert("Producto agregado exitosamente al carrito");
-//         });
-//     });
-// } else {
-//     console.error("Error: El producto no se agregó correctamente al carrito.");
-//     }
-//     updateCart();
-// }
-
-// function addToCart(productName, productPrice) {
-//     const existingProduct = cart.find(item => item.name === productName);
-//     if (existingProduct) {
-//         existingProduct.quantity++;
-//     } else {
-//         cart.push({ name: productName, price: productPrice, quantity: 1 });
-//     }
-
-//     // Verificar si el producto se agregó correctamente al carrito
-//     const productAdded = cart.some(item => item.name === productName);
-
-//     if (productAdded) {
-//         const avisos = document.querySelectorAll(".aviso");
-
-//         avisos.forEach(aviso => {
-//             aviso.addEventListener("click", (event) => {
-//                 event.preventDefault();
-//                 alert("Producto agregado exitosamente al carrito");
-//             });
-//         });
-//     } else {
-//         console.error("Error: El producto no se agregó correctamente al carrito.");
-//     }
-
-//     updateCart(); // Actualizar el carrito después de agregar el producto
-// }
-
-    
-// function removeFromCart(productName) {
-//     cart = cart.filter(item => item.name !== productName);
-//     updateCart();
-// }
-    
-// function updateQuantity(productName, quantity) {
-//     const product = cart.find(item => item.name === productName);
-//     if (product) {
-//         product.quantity = parseInt(quantity, 10);
-//         if (product.quantity <= 0) {
-//             removeFromCart(productName);
-//         }
-//     }
-//     updateCart();
-// }
-    
-// function updateCart() {
-// const cartItems = document.getElementById('cartItems');
-// cartItems.innerHTML = '';
-  
-//     let total = 0;
-//     let totalItems = 0;
-    
-//     cart.forEach((item) => {
-//         const listItem = document.createElement('li');
-//         listItem.className = 'list-group-item d-flex justify-content-between align-items-center';
-//         listItem.innerHTML = `
-//             ${item.name} - $${item.price} x 
-//             <input type="number" class="quantity-input form-control" value="${item.quantity}" min="1" onchange="updateQuantity('${item.name}', this.value)" style="width: 60px;">
-//             <button class="btn btn-danger btn-sm" onclick="removeFromCart('${item.name}')">Eliminar</button>
-//         `;
-//         cartItems.appendChild(listItem);
-    
-//         total += item.price * item.quantity;
-//         totalItems += item.quantity;
-//     });
-    
-//     document.getElementById('total').textContent = total.toFixed(2);
-//     document.getElementById('cartCount').textContent = totalItems;
-// }
-    
-// // Event listener for cart modal close to reset the form
-// document.getElementById('cartModal').addEventListener('hidden.bs.modal', function () {
-//     updateCart();
-// });
-    
-
-
+// funcion para actualizar el numero de elementos de un carrito
+function updateCartCount() {
+    const cartCount = document.getElementById('cartCount');
+    const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
+    cartCount.innerText = totalItems;
+}
 
 function addToCart(productName, productPrice) {
     const existingProduct = cart.find(item => item.name === productName);
@@ -131,42 +42,44 @@ function addToCart(productName, productPrice) {
     } else {
         cart.push({ name: productName, price: productPrice, quantity: 1 });
     }
-
-    console.log('Producto agregado al carrito:', productName); // Verificar en la consola
-
+    localStorage.setItem('cart', JSON.stringify(cart));
     updateCart(); // Actualizar visualmente el carrito después de modificarlo
-
+    updateCartCount();
+    
     // Mostrar mensaje de éxito al agregar al carrito
     alert("Producto agregado exitosamente al carrito");
+    console.log('Producto agregado al carrito:', productName); // Verificar en la consola
 }
 
 function updateCart() {
-    const cartItems = document.getElementById('cartItems');
-    cartItems.innerHTML = ''; // Limpiar contenido anterior del carrito
+    const cartItems = document.getElementById('cart-items');
+    if (cartItems) {
+        cartItems.innerHTML = '';
+        let total = 0;
 
-    let total = 0;
+        cart.forEach((item) => {
+            const listItem = document.createElement('li');
+            listItem.className = 'list-group-item d-flex justify-content-between align-items-center';
+            listItem.innerHTML = `
+                ${item.name} - $${item.price} x 
+                <input type="number" class="quantity-input form-control" value="${item.quantity}" min="1" onchange="updateQuantity('${item.name}', this.value)" style="width: 60px;">
+                <button class="btn btn-danger btn-sm" onclick="removeFromCart('${item.name}')">Eliminar</button>
+            `;
+            cartItems.appendChild(listItem);
+            total += item.price * item.quantity;
+        });
 
-    cart.forEach((item, index) => {
-        const listItem = document.createElement('li');
-        listItem.className = 'list-group-item d-flex justify-content-between align-items-center';
-        listItem.innerHTML = `
-            ${item.name} - $${item.price} x 
-            <input type="number" class="quantity-input form-control" value="${item.quantity}" min="1" onchange="updateQuantity('${item.name}', this.value)" style="width: 60px;">
-            <button class="btn btn-danger btn-sm" onclick="removeFromCart('${item.name}')">Eliminar</button>
-        `;
-        cartItems.appendChild(listItem);
-
-        total += item.price * item.quantity;
-    });
-
-    // Mostrar el total en el carrito
-    document.getElementById('cartTotal').innerText = `$${total.toFixed(2)}`;
+        document.getElementById('cartTotal').innerText = `$${total.toFixed(2)}`;
+    }
+    updateCartCount();
 }
 
 // Ejemplo de función para eliminar un producto del carrito
 function removeFromCart(productName) {
     cart = cart.filter(item => item.name !== productName);
+    localStorage.setItem('cart', JSON.stringify(cart));
     updateCart();
+    updateCartCount();
 }
 
 // Ejemplo de función para actualizar la cantidad de un producto en el carrito
@@ -178,18 +91,21 @@ function updateQuantity(productName, quantity) {
             removeFromCart(productName);
         }
     }
+    localStorage.setItem('cart', JSON.stringify(cart));
     updateCart();
+    updateCartCount();
 }
 
 // Añadir eventos de click a los botones de agregar al carrito
 const avisos = document.querySelectorAll(".aviso");
 
-avisos.forEach(aviso => {
-    aviso.addEventListener("click", (event) => {
-        event.preventDefault();
-        const productName = aviso.getAttribute('data-product-name');
-        const productPrice = parseFloat(aviso.getAttribute('data-product-price'));
-        addToCart(productName, productPrice);
+avisos.forEach(button => {
+    button.addEventListener('click', function() {
+        const productName = this.getAttribute('data-product-name');
+        const productPrice = parseFloat(this.getAttribute('data-product-price'));
+        
     });
 });
+
+
 
